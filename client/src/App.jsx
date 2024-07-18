b28ff778f37aa010b022ae4574206c43b2b7369d
SOULBUSTER <shreyashshindegs@gmail.com>
2024-07-18 22:54:28 +0530

    frst commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const App = props => {
+  return (
+    <h1 className='text-red-500'>Hello </h1>
+  )
+}
+
+App.propTypes = {}
+
+export default App
