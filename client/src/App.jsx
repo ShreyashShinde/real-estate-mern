30f22e62120bc81c910b7c87a500257a07954317
SOULBUSTER <shreyashshindegs@gmail.com>
2024-07-19 00:20:25 +0530

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUP from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 const App = props => {
   return (
-    <h1 className='text-red-500'>Hello </h1>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/sign-in" element={<SignIn/>}></Route>
+        <Route path="/sign-up" element={<SignUP/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
